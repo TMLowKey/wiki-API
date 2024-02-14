1ce9970542c60372bbf6368b2afe4684021aaafe
Lowkey <marek.jtengler@gmail.com>
2024-02-14 14:24:56 +0100

    feat: add responses
    api reacting on fuzzy search, multiple article choices and not founding search_term

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 import wikipedia
 import requests
+import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/wiki/<search_term>', methods=['GET'])
 def get_wikipedia_info(search_term):
-    #language = request.headers.get(Accept-Language)
+    # Setting up language of wikipedia quarry, default value "cs"
     language = request.args.get('Accept-Language')
-    
     if language:
         wikipedia.set_lang(language)
     else:
         wikipedia.set_lang("cs")
 
     try:
-        wikipedia_response = wikipedia.summary(search_term, auto_suggest=False)
+        # Check if the search_term have wiki page byitself or it just appear in artle, 
+        #   also check if the list is not empty otherwise method .summary return error
+        if (len(wikipedia.search(search_term)) > 0) and (search_term.lower() != wikipedia.search(search_term)[0].lower()):
+            wikipedia_response = {'message': "Search term is appeared in other articles", 'data':wikipedia.search(search_term), 'Code': 303}
+        else:
+            wikipedia_response = {'message': "Search term found", 'data':wikipedia.summary(search_term, auto_suggest = False), 'Code':200}
     except wikipedia.exceptions.DisambiguationError as e:
-        return e.options, 303
+        # Appear when wiki API dont find term but there is suggested terms
+        return {'message': "Did you meant", 'data': e.options, 'Code': 404}
     except wikipedia.exceptions.PageError as e:
-        return "No content found", 404
-
-    return wikipedia_response, 200
+        # Appear when there is no wikipedia suggested article
+        return {'message': "Search term not found", 'data': None, 'Code': 404}
+    
+    return json.dumps(wikipedia_response, ensure_ascii=False),  200
 
 
 @app.errorhandler(requests.exceptions.ConnectionError)
